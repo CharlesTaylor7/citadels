2a630ae6325f7eebf1aab549d9cecd86a5061cb2
Charles Taylor <charlestaylor95@gmail.com>
2023-12-25 20:37:44 -0500

    load html for lobby

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,11 @@ import Data.Foldable (fold)
 import Web.Scotty qualified as Scotty
 import Text.Blaze.Html qualified as Blaze 
 import Text.Blaze.Html.Renderer.Text qualified as Blaze
+import Network.Wai.Middleware.Static qualified as Wai
 
+main :: IO ()
 main = Scotty.scotty 8080 $ do
+    Scotty.middleware $ Wai.static
     Scotty.get "/" $ do
-      Scotty.html $ Blaze.renderHtml "Hello, World!"
+      Scotty.redirect "/public/index.html"
+      --Scotty.html $ Blaze.renderHtml "Hello, World!"
