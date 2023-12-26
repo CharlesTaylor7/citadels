beee0cb46a9ef966a0476194849f69e45755b581
Charles Taylor <charlestaylor95@gmail.com>
2023-12-25 20:14:46 -0500

    Setup scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Prelude
+import Web.Scotty
+
+main = scotty 8080 $ do
+    get "/" $ do
+      html "Hello, World!"
+
+    get "/:word" $ do
+        beam <- pathParam "word"
+        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
