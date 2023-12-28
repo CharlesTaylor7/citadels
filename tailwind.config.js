9f5a374254f48177f05afc9b7c08f8dfdaa7c40a
Charles Taylor <charlestaylor95@gmail.com>
2023-12-28 11:46:45 -0500

    Convert lucid to jinja

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: { 
-    files: ["src/**/*.rs", "templates/**/*.j2"],
+    files: ["src/**/*.rs", "templates/**/*.j2", "templates/**/*.html"],
   },
   theme: {
     extend: {
