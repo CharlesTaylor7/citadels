63ff35701ee824428dd560e37ca7af6222faebdc
Charles Taylor <charlestaylor95@gmail.com>
2023-12-28 18:27:45 -0500

    working ws

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,56 @@
 use axum::{
-    extract::{
-        ws::{Message, WebSocket},
-        FromRef,
-    },
+    extract::{ws::Message, FromRef},
+    response::Html,
     routing::{get, post},
-    Router,
+    Error, Router,
 };
 use axum_extra::extract::cookie;
 use citadels::lobby::Lobby;
-use futures::stream::SplitSink;
 use load_dotenv::load_dotenv;
 use minijinja;
 use std::sync::{Arc, Mutex};
 use std::{collections::hash_map::HashMap, sync::RwLock};
-use tokio;
+use tokio::{self, sync::mpsc};
 use tower_http::services::ServeDir;
 
 // TODO:
-// - [x] run on 8080
-// - [x] serve public/index.css
-// - [x] load jinja templates
-// - [x] cookies
-// - routes
-// - [x] /
-// - [x] /register
-// - [x] /ws
 // - [ ] /start
 
+type WebSocketSink = mpsc::UnboundedSender<Result<Message, Error>>;
+
 #[derive(Clone)]
-pub struct Context {
+pub struct AppState {
     pub cookie_signing_key: cookie::Key,
     pub jinja_env: minijinja::Environment<'static>,
     pub lobby: Arc<RwLock<Lobby>>,
     pub connections: Arc<Mutex<HashMap<String, WebSocketSink>>>,
 }
 
-type WebSocketSink = SplitSink<WebSocket, Message>;
+impl AppState {
+    fn template<'a, S: serde::Serialize>(&self, template: &'a str, ctx: S) -> Html<String> {
+        Html(
+            self.jinja_env
+                .get_template(template)
+                .unwrap()
+                .render(ctx)
+                .unwrap(),
+        )
+    }
 
-impl FromRef<Context> for cookie::Key {
-    fn from_ref(context: &Context) -> Self {
-        context.cookie_signing_key.clone()
+    pub fn broadcast(&mut self, html: Html<String>) {
+        self.connections
+            .lock()
+            .unwrap()
+            .iter_mut()
+            .for_each(|(_key, ws)| {
+                let _ = ws.send(Ok(Message::Text(html.0.clone())));
+            });
+    }
+}
+
+impl FromRef<AppState> for cookie::Key {
+    fn from_ref(state: &AppState) -> Self {
+        state.cookie_signing_key.clone()
     }
 }
 
@@ -65,7 +76,7 @@ fn router() -> Router {
                 .unwrap();
         }
     }
-    let context = Context {
+    let context = AppState {
         cookie_signing_key: cookie::Key::from(env!("COOKIE_SIGNING_KEY").as_bytes()),
         jinja_env: env,
         lobby: Arc::new(RwLock::new(Lobby::default())),
@@ -76,23 +87,14 @@ fn router() -> Router {
         .route("/", get(handlers::index))
         .route("/register", post(handlers::register))
         .route("/ws", get(handlers::ws))
+        .route("/start", get(handlers::start))
         .nest_service("/public", ServeDir::new("public"))
         .with_state(context)
 }
 
-fn template<'a, S: serde::Serialize>(
-    env: &minijinja::Environment,
-    template: &'a str,
-    ctx: S,
-) -> axum::response::Html<String> {
-    use axum::response::Html;
-
-    Html(env.get_template(template).unwrap().render(ctx).unwrap())
-}
-
 mod handlers {
 
-    use crate::Context;
+    use crate::AppState;
 
     use axum::extract::State;
 
@@ -107,7 +109,7 @@ mod handlers {
     use uuid::Uuid;
 
     pub async fn index(
-        state: State<Context>,
+        app: State<AppState>,
         cookies: PrivateCookieJar,
     ) -> impl axum::response::IntoResponse {
         let username = cookies
@@ -117,7 +119,7 @@ mod handlers {
         let player_id = cookies.get("playerId").map(|c| c.value().to_owned());
 
         let players: Vec<Player> = {
-            let lobby = state.lobby.read().unwrap();
+            let lobby = app.lobby.read().unwrap();
             lobby
                 .seating
                 .iter()
@@ -131,8 +133,7 @@ mod handlers {
                 "playerId",
                 player_id.unwrap_or_else(|| Uuid::new_v4().to_string()),
             )),
-            crate::template(
-                &state.jinja_env,
+            app.template(
                 "lobby.html",
                 context!(
                     username => username,
@@ -149,7 +150,7 @@ mod handlers {
     }
 
     pub async fn register(
-        state: State<Context>,
+        mut app: State<AppState>,
         cookies: PrivateCookieJar,
         args: axum::Form<Register>,
     ) -> impl IntoResponse {
@@ -157,7 +158,7 @@ mod handlers {
         let cookie = cookies.get("playerId").unwrap();
         let player_id = cookie.value();
         let players: Vec<Player> = {
-            let mut lobby = state.lobby.write().unwrap();
+            let mut lobby = app.lobby.write().unwrap();
             match lobby.players.entry(player_id.to_owned()) {
                 Entry::Occupied(e) => {
                     e.into_mut().name = args.username.clone();
@@ -177,21 +178,26 @@ mod handlers {
                 .cloned()
                 .collect()
         };
+        let html = app.template(
+            "lobby.html#players",
+            context!(
+                players => players,
+            ),
+        );
+        app.broadcast(html);
 
         (
             cookies.add(Cookie::new("username", args.username.clone())),
-            crate::template(
-                &state.jinja_env,
-                "lobby.html#players",
-                context!(
-                    players => players,
-                ),
-            ),
+            "",
         )
     }
 
+    pub async fn start(_state: State<AppState>, _cookies: PrivateCookieJar) -> impl IntoResponse {
+        (StatusCode::NOT_FOUND, "wip")
+    }
+
     pub async fn ws(
-        state: State<Context>,
+        state: State<AppState>,
         cookies: PrivateCookieJar,
         ws: WebSocketUpgrade,
     ) -> impl IntoResponse {
@@ -210,17 +216,24 @@ pub mod ws {
     use axum::extract::ws::{Message, WebSocket};
     use axum::extract::State;
     use futures::stream::StreamExt;
+    use tokio::sync::mpsc::{self};
+    use tokio_stream::wrappers::UnboundedReceiverStream;
 
     use std::ops::ControlFlow;
 
-    use crate::Context;
-    pub async fn handle_socket(state: State<Context>, player_id: String, socket: WebSocket) {
-        let (sender, mut receiver) = socket.split();
-        {
-            let mut connections = state.connections.lock().unwrap();
-            connections.insert(player_id, sender);
-        }
-        while let Some(Ok(msg)) = receiver.next().await {
+    use crate::AppState;
+    pub async fn handle_socket(state: State<AppState>, player_id: String, socket: WebSocket) {
+        let (ws_sender, mut ws_recv) = socket.split();
+        let (chan_sender, chan_recv) = mpsc::unbounded_channel();
+        tokio::spawn(UnboundedReceiverStream::new(chan_recv).forward(ws_sender));
+
+        state
+            .connections
+            .lock()
+            .unwrap()
+            .insert(player_id, chan_sender);
+
+        while let Some(Ok(msg)) = ws_recv.next().await {
             if process_message(msg).is_break() {
                 break;
             }
