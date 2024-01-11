649fff6c904dc62a0e0bfda3c527ffa703b9ad23
Charles Taylor <charlestaylor95@gmail.com>
2024-01-11 09:20:58 -0500

    Cleanup example

--- macros/src/tag.rs
+++ macros/src/tag.rs
@@ -2,7 +2,6 @@ pub use macros_impl::Tag;
 
 /// Derivable for any enum
 /// Creates a new enum with unit variants.
-/// Creates a new struct for each struct variant.
 /// ```
 /// #[derive(Tag)]
 /// enum Foo {
@@ -23,43 +22,11 @@ pub use macros_impl::Tag;
 ///         }
 ///     }
 /// }
-///
-/// mod Foo_singletons {
-///     pub enum Bar {}
-/// }
-///
-/// struct BarArgs {
-///     a: usize,
-///     b: String,
-/// }
-///
-/// impl TagArgs for BarArgs {
-///     type Target = Foo;
-///     fn build(self) -> Foo {
-///         Foo::Bar { a: self.a, b: self.b }
-///     }
-/// }
 /// ```
-///
 pub trait Tag: Sized {
     type Tag: std::fmt::Debug + Clone + Copy + PartialEq + Eq;
 
     fn tag(&self) -> Self::Tag;
-    // fn from_dyn(tag: Self::Tag, args: Dynamic) -> Self;
-}
-/*
-pub trait TagArgs {
-    type Target;
-    type Tag;
-    fn build(self) -> Self::Target;
 }
 
-*/
-/*
-// TODO: use from_tag
-macro_rules! build {
-    () => {
 
-    };
-}
-*/
