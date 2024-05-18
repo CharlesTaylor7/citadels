d98cef9498b3f55bbac559684496d52c13034d40
Charles Taylor <charlestaylor95@gmail.com>
2024-05-17 20:55:25 -0400

    Fix build

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use citadels::server::routes::get_router;
 
 #[tokio::main]
 async fn main() {
-    if cfg!(feature = "dotenv") {
-        dotenv::dotenv().expect(".env not found");
-    }
+    #[cfg(feature = "dotenv")]
+    dotenv::dotenv().expect(".env not found");
     citadels::logger::init();
 
     let port = "0.0.0.0:8080";
